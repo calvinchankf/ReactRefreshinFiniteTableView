3add2f9de07722e28828ed9cfd7cc52672629a39
calvinchankf <chan9118kin@gmail.com>
2016-06-02 00:14:09 +0800

    minor changes

--- server.js
+++ server.js
@@ -3,42 +3,32 @@ var express = require('express');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 
-var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = process.env.PORT || 3000 ;
 var app = express();
 
-if (isDeveloping) {
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
-  var webpackMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
+var compiler = webpack(webpackConfig);
+var middleware = webpackMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
 
-  var compiler = webpack(webpackConfig);
-  var middleware = webpackMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
-
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-  });
-
-} else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+app.use(express.static(__dirname + '/dist'));
+app.get('*', function response(req, res) {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+});
 
 app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
